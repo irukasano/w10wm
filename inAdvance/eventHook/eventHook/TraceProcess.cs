a26be9d4e3eedb4d61e59bb43306c4f33f47e536
SANO,Takeshi <sano@iruka-system.co.jp>
2020-10-07 17:52:28 +0900

    ウィンドウの作成（表示）、終了を補足するテスト

--- inAdvance/eventHook/eventHook/TraceProcess.cs
+++ inAdvance/eventHook/eventHook/TraceProcess.cs
@@ -26,6 +26,18 @@ namespace eventHook
         const uint OBJID_ALERT = 0xFFFFFFF6;
         const uint OBJID_SOUND = 0xFFFFFFF5;
 
+        // for GetWindowLong
+        const int GWL_STYLE = (-16);
+        const UInt32 WS_POPUP = 0x80000000;
+        const UInt32 WS_CHILD = 0x40000000;
+        const UInt32 WS_VISIBLE = 0x10000000;
+        const UInt32 WS_DISABLED = 0x08000000;
+        const UInt32 WS_CAPTION = 0x00C00000;
+        const UInt32 WS_OVERLAPPEDWINDOW = 0x00CF0000;
+
+        // for GetWindow
+        const uint GW_OWNER = 4;
+
         public class OriginalWinEventArg : EventArgs
         {
         }
@@ -33,46 +45,72 @@ namespace eventHook
         [DllImport("user32.dll", SetLastError = true)]
         static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
 
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern UInt32 GetWindowLong(IntPtr hWnd, int nIndex);
+
         public delegate void WinEventHandler(object sender, OriginalWinEventArg e);
         //public event WinEventHandler FocusEvent;
         // public event WinEventHandler ResizeEvent;
+
+        protected List<IntPtr> WindowHandles { get; set; } = new List<IntPtr>();
 
         public override void HookProcedure(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
         {
             var eventName = EventMapConverter.CodeToName((int)eventType);
 
+            if ((uint)idObject != OBJID_WINDOW)
+            {
+                return;
+            }
             if (eventName == EventName.UNKNOWN)
             {
                 return;
             }
-            if (eventName == EventName.EVENT_OBJECT_LOCATIONCHANGE)
+            var windowLong = GetWindowLong(hwnd, GWL_STYLE);
+            var windowTitle = GetCurrentWindowTitle(hwnd);
+            var is_visible = (windowLong & WS_VISIBLE) == WS_VISIBLE;
+            var is_overlappedwindow = (windowLong & WS_OVERLAPPEDWINDOW) == WS_OVERLAPPEDWINDOW;
+
+            //Debug.WriteLine( windowTitle + " : " + windowLong.ToString("x8"));
+
+            if ( windowLong == 0)
+            {
+                return;
+            }
+            if ((windowLong & WS_CHILD) == WS_CHILD)
             {
                 return;
             }
             /*
-            if ( eventName == EventName.EVENT_OBJECT_VALUECHANGE)
-            {
-                return;
-            }
-            if (eventName == EventName.EVENT_OBJECT_REORDER)
+            if ((windowLong & WS_POPUP ) == WS_POPUP)
             {
                 return;
             }
             */
 
+
+            if (is_visible && is_overlappedwindow &&  eventName == EventName.EVENT_OBJECT_SHOW)
+            //if (is_visible && eventName == EventName.EVENT_OBJECT_SHOW)
+            {
+                Debug.WriteLine("Window created: " + windowTitle + " : " + windowLong.ToString("x8"));
+                WindowHandles.Add(hwnd);
+                /*
+                Debug.WriteLine("---");
+                Debug.WriteLine(windowTitle + ":" + is_visible + ":" + eventName + ":" + windowLong);
+                Debug.WriteLine("WS_VISIBLE = " + ((windowLong & WS_VISIBLE) == WS_VISIBLE));
+                Debug.WriteLine("WS_OVERLAPPEDWINDOW = " + ((windowLong & WS_OVERLAPPEDWINDOW) == WS_OVERLAPPEDWINDOW));
+                */
+            }
+            else if ( eventName == EventName.EVENT_OBJECT_DESTROY)
+            {
+                if (WindowHandles.Contains(hwnd))
+                {
+                    Debug.WriteLine("Window destroyed: " + windowTitle + " : " + windowLong.ToString("x8"));
+                    WindowHandles.Remove(hwnd);
+                }
+            }
+
             /*
-            if (idChild != 0)
-            {
-                return;
-            }
-            */
-            if ((uint)idObject != OBJID_ALERT &&
-                (uint)idObject != OBJID_WINDOW)
-            {
-                return;
-            }
-
-
             Debug.WriteLine("----");
             Debug.WriteLine("hWinEventHook:" + hWinEventHook);
             Debug.WriteLine("eventType:"+eventName);
@@ -82,8 +120,8 @@ namespace eventHook
             Debug.WriteLine("idChild:" + idChild);
             Debug.WriteLine("dwEventThread:" + dwEventThread);
             Debug.WriteLine("dwmsEventTime:" + dwmsEventTime);
+            */
         }
-
 
         private string GetCurrentWindowTitle(IntPtr hwnd)
         {
