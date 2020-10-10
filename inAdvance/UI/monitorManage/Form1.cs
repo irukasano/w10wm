83ec32182e111b72bb905177d0f77318a7c4f603
SANO,Takeshi <sano@iruka-system.co.jp>
2020-10-11 00:28:08 +0900

    モニター情報の取得テスト

--- inAdvance/UI/monitorManage/Form1.cs
+++ inAdvance/UI/monitorManage/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,33 @@ namespace monitorManage
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MonitorManager monitorManager = new MonitorManager();
+            MonitorInfoWithHandle[] monitorInfoWithHandle = monitorManager.GetMonitors();
+
+            for (int i = 0; i < monitorInfoWithHandle.Length; i++)
+            {
+                Debug.WriteLine("---");
+                //Debug.WriteLine(monitorInfoWithHandle[i].MonitorHandle);
+                Debug.WriteLine(new String(monitorInfoWithHandle[i].MonitorInfo.szDevice));
+                Debug.WriteLine("");
+                Debug.WriteLine(monitorInfoWithHandle[i].MonitorInfo.work.left.ToString());
+                Debug.WriteLine(monitorInfoWithHandle[i].MonitorInfo.work.top.ToString());
+                Debug.WriteLine(monitorInfoWithHandle[i].MonitorInfo.work.right.ToString());
+                Debug.WriteLine(monitorInfoWithHandle[i].MonitorInfo.work.bottom.ToString());
+            }
+
+            List<MonitorInfoWithHandle> monitors = monitorManager.MonitorInfos;
+
+            foreach (var monitor in monitors)
+            {
+                Debug.WriteLine(new String(monitor.MonitorInfo.szDevice));
+                Debug.WriteLine("");
+            }
+
         }
     }
 }
---
+++ inAdvance/UI/monitorManage/MonitorManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace monitorManage
+{
+
+    class MonitorManager
+    {
+
+        /// <summary>
+        /// Monitor Enum Delegate
+        /// </summary>
+        /// <param name="hMonitor">A handle to the display monitor.</param>
+        /// <param name="hdcMonitor">A handle to a device context.</param>
+        /// <param name="lprcMonitor">A pointer to a RECT structure.</param>
+        /// <param name="dwData">Application-defined data that EnumDisplayMonitors passes directly to the enumeration function.</param>
+        /// <returns></returns>
+        public delegate bool MonitorEnumDelegate(IntPtr hMonitor, IntPtr hdcMonitor,
+            ref RECT lprcMonitor, IntPtr dwData);
+
+        /// <summary>
+        /// Enumerates through the display monitors.
+        /// </summary>
+        /// <param name="hdc">A handle to a display device context that defines the visible region of interest.</param>
+        /// <param name="lprcClip">A pointer to a RECT structure that specifies a clipping rectangle.</param>
+        /// <param name="lpfnEnum">A pointer to a MonitorEnumProc application-defined callback function.</param>
+        /// <param name="dwData">Application-defined data that EnumDisplayMonitors passes directly to the MonitorEnumProc function.</param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip,
+            MonitorEnumDelegate lpfnEnum, IntPtr dwData);
+
+        /// <summary>
+        /// Gets the monitor information.
+        /// </summary>
+        /// <param name="hmon">A handle to the display monitor of interest.</param>
+        /// <param name="mi">A pointer to a MONITORINFO instance created by this method.</param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        static extern bool GetMonitorInfo(IntPtr hmon, ref MONITORINFO mi);
+
+        public List<MonitorInfoWithHandle> MonitorInfos { get; set; }
+
+
+        /// <summary>
+        /// Monitor Enum Delegate
+        /// </summary>
+        /// <param name="hMonitor">A handle to the display monitor.</param>
+        /// <param name="hdcMonitor">A handle to a device context.</param>
+        /// <param name="lprcMonitor">A pointer to a RECT structure.</param>
+        /// <param name="dwData">Application-defined data that EnumDisplayMonitors passes directly to the enumeration function.</param>
+        /// <returns></returns>
+        public bool MonitorEnum(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData)
+        {
+            var mi = new MONITORINFO();
+            mi.size = (uint)Marshal.SizeOf(mi);
+            GetMonitorInfo(hMonitor, ref mi);
+
+            // Add to monitor info
+            MonitorInfos.Add(new MonitorInfoWithHandle(hMonitor, mi));
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the monitors.
+        /// </summary>
+        /// <returns></returns>
+        public MonitorInfoWithHandle[] GetMonitors()
+        {
+            // New List
+            MonitorInfos = new List<MonitorInfoWithHandle>();
+
+            // Enumerate monitors
+            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, MonitorEnum, IntPtr.Zero);
+
+            return MonitorInfos.ToArray();
+        }
+
+    }
+}
