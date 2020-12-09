296fb86b83d225d9a5257114177ac70eff68a993
SANO,Takeshi <sano@iruka-system.co.jp>
2020-12-09 23:12:01 +0900

    WindowTilerDividerBugn の実装、テスト

--- windows10windowManager/Window/WindowTilerDividerBugn.cs
+++ windows10windowManager/Window/WindowTilerDividerBugn.cs
@@ -4,9 +4,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using windows10windowManager.Util;
+
 namespace windows10windowManager.Window
 {
-    public class WindowTilerDividerBugn : WindowTilerDivider
+    public class WindowTilerDividerBugn : AbstractWindowTiler
     {
+        /**
+         * <summary>
+         * 左側列の横幅％
+         * </summary>
+         */
+        public double percentOfLeftColumn = 0.65;
+
+        /**
+         * <summary>
+         * 右側列に縦に並べる最大ウィンドウ数
+         * </summary>
+         */
+        public int maxCountOfWindowOfRightColumn = 6;
+
+        /**
+         * <summary>
+         * 左は１ウィンドウ、右は複数ウィンドウ
+         * </summary>
+         */
+        public override void CalcuratePosition(int windowCount, int monitorTop, int monitorBottom, int monitorLeft, int monitorRight)
+        {
+            if ( windowCount == 0)
+            {
+                return;
+            }
+            
+            // 定義すべきウィンドウ位置の最大数は、
+            // 右側の複数ウィンドウ＋１（左側）だが
+            // 右側はウィンドウ数によって可変
+            var countOfWindowOfRightColumn = this.maxCountOfWindowOfRightColumn;
+            if ( windowCount -1 < countOfWindowOfRightColumn)
+            {
+                countOfWindowOfRightColumn = windowCount - 1;
+            }
+
+            // 左側列の定義
+            var monitorWidth = monitorRight - monitorLeft;
+            var monitorHeight = monitorBottom - monitorTop;
+            var windowWidthOfLeftColumn = (int)Math.Floor(monitorWidth * this.percentOfLeftColumn);
+            this.windowRects.Add(new WindowRect(
+                /* top     = */ monitorTop,
+                /* bottom  = */ monitorBottom,
+                /* left    = */ monitorLeft,
+                /* right   = */ monitorLeft + windowWidthOfLeftColumn
+            ));
+
+            if ( countOfWindowOfRightColumn == 0 )
+            {
+                // 右側列にウィンドウがなければここで計算終了
+                return;
+            }
+
+            // 右側列の定義
+            var windowWidthOfRightColumn = monitorWidth - windowWidthOfLeftColumn;
+            var windowHeightOfRightColumn = monitorHeight / countOfWindowOfRightColumn;
+            var windowTop = monitorTop;
+            var windowLeft = monitorLeft + windowWidthOfLeftColumn;
+            var windowRight = windowLeft + windowWidthOfRightColumn;
+            for (int i = 0; i < countOfWindowOfRightColumn; i++)
+            {
+                var windowBottom = windowTop + windowHeightOfRightColumn;
+                var newWindowRect = new WindowRect(
+                    /* top     = */ windowTop,
+                    /* bottom  = */ windowBottom,
+                    /* left    = */ windowLeft,
+                    /* right   = */ windowRight
+                );
+
+                Logger.WriteLine(newWindowRect.ToString());
+
+                this.windowRects.Add(newWindowRect);
+
+                windowTop = windowBottom;
+            }
+
+        }
     }
 }
---
+++ windows10windowManagerTests/Window/WindowTilerDividerBugn.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+using windows10windowManager.Window;
+
+namespace windows10windowManagerTests.Window
+{
+    [TestClass]
+    public class WindowTilerDividerBugnTest
+    {
+        [TestMethod]
+        public void Test_GetWindowRectOf_NoWindow()
+        {
+            WindowTilerDividerBugn windowTiler = new WindowTilerDividerBugn();
+
+            windowTiler.CalcuratePosition(0, 0, 1000, 0, 2000);
+            Assert.IsTrue(windowTiler.defaultWindowRect.Equals(windowTiler.GetWindowRectOf(0)));
+        }
+
+        [TestMethod]
+        public void Test_GetWindowRectOf_CountOfRightStackWindowIsZero()
+        {
+            WindowTilerDividerBugn windowTiler = new WindowTilerDividerBugn();
+            windowTiler.percentOfLeftColumn = 0.7;
+
+            windowTiler.CalcuratePosition(1, 0, 1000, 0, 2000);
+            Assert.IsTrue(new WindowRect(0, 1000, 0, 1400).Equals(windowTiler.GetWindowRectOf(0)));
+        }
+
+        [TestMethod]
+        public void Test_GetWindowRectOf_CountOfRightStackWindowIsOne()
+        {
+            WindowTilerDividerBugn windowTiler = new WindowTilerDividerBugn();
+            windowTiler.percentOfLeftColumn = 0.7;
+            windowTiler.maxCountOfWindowOfRightColumn = 6;
+
+            windowTiler.CalcuratePosition(2, 0, 1000, 0, 2000);
+            Assert.IsTrue(new WindowRect(0, 1000, 0, 1400).Equals(windowTiler.GetWindowRectOf(0)));
+            Assert.IsTrue(new WindowRect(0, 1000, 1400, 2000).Equals(windowTiler.GetWindowRectOf(1)));
+        }
+
+        [TestMethod]
+        public void Test_GetWindowRectOf_CountOfRightStackWindowIsThree()
+        {
+            WindowTilerDividerBugn windowTiler = new WindowTilerDividerBugn();
+            windowTiler.percentOfLeftColumn = 0.7;
+            windowTiler.maxCountOfWindowOfRightColumn = 6;
+
+            windowTiler.CalcuratePosition(5, 0, 1000, 0, 2000);
+            Assert.IsTrue(new WindowRect(0, 1000, 0, 1400).Equals(windowTiler.GetWindowRectOf(0)));
+            Assert.IsTrue(new WindowRect(0, 250, 1400, 2000).Equals(windowTiler.GetWindowRectOf(1)));
+            Assert.IsTrue(new WindowRect(250, 500, 1400, 2000).Equals(windowTiler.GetWindowRectOf(2)));
+            Assert.IsTrue(new WindowRect(500, 750, 1400, 2000).Equals(windowTiler.GetWindowRectOf(3)));
+            Assert.IsTrue(new WindowRect(750, 1000, 1400, 2000).Equals(windowTiler.GetWindowRectOf(4)));
+        }
+
+        [TestMethod]
+        public void Test_GetWindowRectOf_CountOfRightStackWindowIsOverMax()
+        {
+            WindowTilerDividerBugn windowTiler = new WindowTilerDividerBugn();
+            windowTiler.percentOfLeftColumn = 0.7;
+            windowTiler.maxCountOfWindowOfRightColumn = 4;
+
+            windowTiler.CalcuratePosition(8, 0, 1000, 0, 2000);
+            Assert.IsTrue(new WindowRect(0, 1000, 0, 1400).Equals(windowTiler.GetWindowRectOf(0)));
+            Assert.IsTrue(new WindowRect(0, 250, 1400, 2000).Equals(windowTiler.GetWindowRectOf(1)));
+            Assert.IsTrue(new WindowRect(250, 500, 1400, 2000).Equals(windowTiler.GetWindowRectOf(2)));
+            Assert.IsTrue(new WindowRect(500, 750, 1400, 2000).Equals(windowTiler.GetWindowRectOf(3)));
+            Assert.IsTrue(new WindowRect(750, 1000, 1400, 2000).Equals(windowTiler.GetWindowRectOf(4)));
+            Assert.IsTrue(new WindowRect(750, 1000, 1400, 2000).Equals(windowTiler.GetWindowRectOf(5)));
+            Assert.IsTrue(new WindowRect(750, 1000, 1400, 2000).Equals(windowTiler.GetWindowRectOf(7)));
+        }
+    }
+}
+
